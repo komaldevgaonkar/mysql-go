170d39a4e894716ca8e6fb69aa65337e5a1fede3
Komal Devgaonkar <you@example.com>
2020-04-02 16:50:15 +0530

    go code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"mysql-go/db"
 	"net/http"
 
@@ -16,21 +15,17 @@ type employee struct {
 }
 
 func main() {
-	dbconn, err := db.GetMySQLDB()
-	if err != nil {
-		log.Println("Connection Failed to Open")
-	} else {
-		log.Println("Connection Established")
-	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 	http.ListenAndServe(":8000", router)
 }
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	db, err := db.GetMySQLDB()
 	var employees []employee
-	result, err := dbconn.Query("SELECT id, name, city from employee")
+	result, err := db.Query("SELECT id, name, city from employee")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -44,4 +39,23 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 		employees = append(employees, post)
 	}
 	json.NewEncoder(w).Encode(employees)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	db, err := db.GetMySQLDB()
+	result, err := db.Query("SELECT id, name, city FROM employee WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var post employee
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.Name, &post.City)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(post)
 }
